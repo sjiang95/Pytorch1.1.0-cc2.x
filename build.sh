2e8f6fb9d5f1bed2df5ef775af4f2df4a2f5fa52
quan <quan@15-b004tx.ubuntu>
2022-05-01 19:43:55 +0900

    build.sh
    add this file to automatically set environment variables and run the build script
    
    Signed-off-by: quan <quan@15-b004tx.ubuntu>

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+python3 setup.py clean --all
+export CC=/usr/local/gcc-5.4.0/bin/gcc
+export CXX=/usr/local/gcc-5.4.0/bin/g++
+export TORCH_CUDA_ARCH_LIST='2.0' #Important! Set this option to 2.1 which is the real cc of 630M resulting in unsupported architecture error when compiling aten/src/THC libs
+export CUDA_HOME=/use/local/cuda-8.0
+export CUDAHOSTCXX=/usr/local/gcc-5.4.0/bin/gcc
+# export NO_MKLDNN=1
+export USE_DISTRIBUTED=0
+export USE_CUDNN=0
+export USE_NCCL=0
+export USE_SYSTEM_NCCL=0
+export CMAKE_PREFIX_PATH="$(dirname $(which conda))/../" # [anaconda root directory]
+python3 setup.py install

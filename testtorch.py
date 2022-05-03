a4bcefc701b80789aa8de9445f10b2845ce28a3c
quan <quan@15-b004tx.ubuntu>
2022-05-03 23:02:58 +0900

    optimize print info
    Signed-off-by: quan <quan@15-b004tx.ubuntu>

--- testtorch.py
+++ testtorch.py
@@ -8,14 +8,19 @@ if torch.cuda.is_available():
     cc_int, cc_decimal = torch.cuda.get_device_capability()
     cc=cc_int+cc_decimal*0.1
     print(f"CUDA device {torch.cuda.get_device_name()} with compute capability {cc} is availabel.")
+    print()
 
     # tensor
+    print("Creating a tensor on GPU:")
     ten=torch.randn(2,3).cuda()
     print(ten)
+    print()
     del ten
 
     # model
+    print("Creating a model on GPU...")
     model=torchvision.models.resnet50().cuda()
     print(f"{model.__class__.__name__} on device {next(model.parameters()).device} is successfully created.")
+    del model
 else:
     print("CUDA is NOT available.")

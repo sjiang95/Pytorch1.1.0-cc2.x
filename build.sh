8da65c2c7f0cb68a79732d76dc4978af2c66206a
quan <quan@15-b004tx.ubuntu>
2022-05-03 23:04:16 +0900

    comment clean build by default
    Signed-off-by: quan <quan@15-b004tx.ubuntu>

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 setup.py clean --all # uncomment this if you want to execute a clean (re)build
+# python3 setup.py clean --all # uncomment this if you want to execute a clean (re)build
 export CC=/usr/local/gcc-5.4.0/bin/gcc
 export CXX=/usr/local/gcc-5.4.0/bin/g++
 export TORCH_CUDA_ARCH_LIST='2.0' # Important! Set this option to 2.1 which is the real cc of 630M resulting in unsupported architecture error when compiling aten/src/THC libs
@@ -13,5 +13,5 @@ export USE_SYSTEM_NCCL=0
 export CMAKE_PREFIX_PATH="$(dirname $(which conda))/../" # [anaconda root directory]
 export CFLAGS="-Wno-unused-parameter" # suppress "unused parameters warning"
 python3 setup.py bdist_wheel
-# python3 setup.py install
+python3 setup.py install
 echo 0
